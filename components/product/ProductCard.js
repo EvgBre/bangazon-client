3b43119db8053ea6a7ad877f6d5d5963e0944e94
Evan Breland <evan.breland@gmail.com>
2023-07-23 19:00:35 -0500

    index and products page fixed

--- components/product/ProductCard.js
+++ components/product/ProductCard.js
@@ -1,34 +1,93 @@
-import React from 'react';
+/* eslint-disable react/forbid-prop-types */
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 import PropTypes from 'prop-types';
+import React from 'react';
+import { useRouter } from 'next/router';
 import { Card, Button } from 'react-bootstrap';
-import Link from 'next/link';
+import { useAuth } from '../../utils/context/authContext';
+import { deleteProduct } from '../../api/productData';
 
-export default function ProductCard({ productObj }) {
+const ProductCard = ({
+  id,
+  name,
+  productImageUrl,
+  description,
+  price,
+  addedOn,
+  categoryId,
+  sellerId,
+  quantity,
+  onUpdate,
+}) => {
+  const router = useRouter();
+  const { user } = useAuth();
+
+  const deletethisProduct = () => {
+    if (window.confirm('Delete your Product?')) {
+      deleteProduct(id).then(() => onUpdate());
+    }
+  };
   return (
-    <Card style={{ width: '18rem' }} className="productCard">
-      <Card.Img variant="top" src={productObj.image_url} />
-      <Card.Body>
-        <Card.Title>{productObj.title}</Card.Title>
-        <Card.Text>{productObj.price}</Card.Text>
-        <Card.Text>{productObj.seller.first_name}</Card.Text>
-        <Link href={`/products/${productObj.id}`} passHref>
-          <Button variant="primary">Product Details</Button>
-        </Link>
-        <Button variant="primary">Add to Cart</Button>
-      </Card.Body>
-    </Card>
+    <>
+      <div>
+        <Card className="product-card">
+          <Card.Header>{name}</Card.Header>
+          <Card.Body>
+            <img src={productImageUrl} style={{ width: '200px' }} />
+            <div>description:{description}</div>
+            <div>price:{price}</div>
+            <div>quantity:{quantity}</div>
+            <div>datedAdded:{addedOn}</div>
+            <div>category: {categoryId.label}</div>
+            <div>seller: {sellerId.username}</div>
+          </Card.Body>
+          <Card.Footer className="text-white">Seller: {sellerId.username} </Card.Footer>
+          <Button
+            className="product-card-button"
+            onClick={() => {
+              router.push(`/products/${id}`);
+            }}
+          >
+            Product Details
+          </Button>
+
+          {sellerId.uid === user.uid
+            ? (
+              <>
+                <Button
+                  onClick={deletethisProduct}
+                  className="product-card-button"
+                >
+                  Delete
+                </Button>
+                <Button
+                  className="product-card-button"
+                  onClick={() => {
+                    router.push(`/products/edit/${id}`);
+                  }}
+                >
+                  Edit
+                </Button>
+              </>
+            ) : ''}
+        </Card>
+      </div>
+    </>
   );
-}
+};
 
 ProductCard.propTypes = {
-  productObj: PropTypes.shape({
-    id: PropTypes.number,
-    title: PropTypes.string,
-    price: PropTypes.number,
-    seller: PropTypes.shape({
-      first_name: PropTypes.string,
-      last_name: PropTypes.string,
-    }),
-    image_url: PropTypes.string,
-  }).isRequired,
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  productImageUrl: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  quantity: PropTypes.number.isRequired,
+  addedOn: PropTypes.string.isRequired,
+  categoryId: PropTypes.object.isRequired,
+  sellerId: PropTypes.object.isRequired,
+  onUpdate: PropTypes.func.isRequired,
 };
+
+export default ProductCard;
